dbc79b3b908ad9a965ace9f7e5e02de952986c6d
Nathaniel091 <nathaniel091@gmail.com>
2020-10-31 14:01:09 +0100

    i just added social-media-dashboad folder as a git repo(first commit tho)

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// // let toggler = document.querySelector("#toggler");
+// // toggler.className = "white-mode";
+
+// document.getElementById("toggler").addEventListener("click", function() {
+	
+// });
+// // <link rel="stylesheet" type="text/css" href="./css/darkmode.css">
