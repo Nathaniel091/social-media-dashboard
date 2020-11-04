2819b9416455b85eb2cbba104931bcfd1600d647
Nathaniel091 <nathaniel091@gmail.com>
2020-11-04 23:37:10 +0100

    white and darkmode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 // // let toggler = document.querySelector("#toggler");
 // // toggler.className = "white-mode";
+const darkMode = () =>{
+	let headTag = document.head;
+	let linkTag = document.createElement("link");
 
-// document.getElementById("toggler").addEventListener("click", function() {
-	
-// });
+	linkTag.setAttribute("rel", "stylesheet");
+	linkTag.setAttribute("href", "css/darkmode.css");
+	headTag.appendChild(linkTag);
+	console.log(linkTag)
+}
+document.getElementById("toggler").addEventListener("click", darkMode);
 // // <link rel="stylesheet" type="text/css" href="./css/darkmode.css">
