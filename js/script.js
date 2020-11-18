5a4128bdc5b6156cecf9fde15307e4f47e026150
Nathaniel091 <nathaniel091@gmail.com>
2020-11-18 22:47:42 +0100

    Done with site

--- js/script.js
+++ js/script.js
@@ -1,69 +1,40 @@
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!----------------------------*/
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!----------------------------*/
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!----------------------------*/
 
-/*
-	1) This script is not working in chrome mobile (for tablet)
-	2) The darkmode button isn't toggling
-	Remember to fix these
-*/
+	// 1) This script is not working in chrome mobile (for tablet)
+	// 2) The darkmode button isn't toggling
+	// Remember to fix these
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!----------------------------*/
+
+const darkModeLinkTag = document.getElementById("darkModeLinkTag");
+const toggler = document.getElementById("toggler");
+const darkmodePath = "css/darkmode.css";
 
 
-
-// darkmode function
-
-// const darkMode = () =>{
-function darkMode(){
-	let headTag = document.head;
-	let linkTag = document.createElement("link");
-	let toggler = document.getElementById("toggler");
-	let darkModeLink = document.getElementById("darkModeLink");
-	let darkmodePath = "css/darkmode.css";
-
-	toggler.classList.toggle("darkmode");
-	if (darkModeLink.getAttribute("href") === "") {
-		darkModeLink.setAttribute("href", darkmodePath);
+// Darkmode function
+// This function toggles the darkmode button
+function darkmode() {
+	if (darkModeLinkTag.getAttribute("href") === "") {
+		darkModeLinkTag.setAttribute("href", darkmodePath);
 	} else {
-		darkModeLink.setAttribute("href", "");
+		darkModeLinkTag.setAttribute("href", "");
 	}
-	
-	console.log();
-
-
-
-
-	// linkTag.setAttribute("id", "darkModeLink");
-	// linkTag.setAttribute("rel", "stylesheet");
-	// linkTag.setAttribute("href", "");
-	// headTag.appendChild(linkTag);
-
-
-	// if (linkTag.getAttribute("href") === "") {
-		// linkTag.setAttribute("href", darkmodePath);
-	// } else {
-		// linkTag.setAttribute("href", "");
-		
-		// linkTag.setAttribute("href", "null");
-		// headTag.removeChild(linkTag);
-	// }
-	// document.removeChild(element)
-	// console.log(toggler, linkTag)
-	// console.log(linkTag.getAttribute("href"));
-
-	// console.log(darkModeLink.getAttribute("href") === "light")
-	// console.log(darkModeLink)
+	toggler.classList.toggle("darkmode");
 };
+document.getElementById("toggler").addEventListener("click", darkmode);
 
 
+// myContact function
+// This function toggles myContactDetails
+function myContact(){
+	let myContactDetails = document.getElementById("myContactDetails");
 
-
-
-
-
-// onclick, call the darkmode function
-// document.getElementById("toggler").addEventListener("click", darkMode);
-
-// var x = document.getElementById("myBtn");
-var toggler = document.getElementById("toggler");
-if (toggler.addEventListener) {
-  toggler.addEventListener("click", darkMode);
-} else if (toggler.attachEvent) {
-  toggler.attachEvent("onclick", darkMode);
-}
+	if (darkModeLinkTag.getAttribute("href") === "") {
+		myContactDetails.classList.toggle("myContactDetailsLight");
+	} else {
+		myContactDetails.classList.toggle("myContactDetailsDark");
+	}
+};
+document.getElementById("myContact").addEventListener("click", myContact);
